bc6635777a96c95f8799cd52ef6c899957cf86bd
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2020-10-23 12:24:50 -0400

    add logging

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,5 +3,10 @@ template "/etc/environment" do
   mode 0664
   owner "root"
   group "root"
+  e = node['etc_environment']
+  Chef::Log.info "MCT-----"
+  Chef::Log.info e
+  Chef::Log.info e.class
+  Chef::Log.info "MCT-----"
   variables({ :environment => node['etc_environment'] })
 end if node['etc_environment']
